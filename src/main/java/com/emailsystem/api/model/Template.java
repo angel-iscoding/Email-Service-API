020efbc3a7cd1bb668d5b9a801b15b5727992520
angel-iscoding <angel.iscoding@gmail.com>
2025-03-06 19:59:43 -0500

    model: added first models

---
+++ src/main/java/com/emailsystem/api/model/Template.java
@@ -0,0 +1,40 @@
+package com.emailsystem.api.model;
+
+public class Template {
+    private Long id;
+    private Long userId;
+    private String subject;
+    private String body;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
